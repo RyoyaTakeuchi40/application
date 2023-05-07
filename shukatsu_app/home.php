b78f6cefd340628e50943bd6ef23b12e5e431dbf
RyoyaTakeuchi40 <ryoya.takeuchi@icloud.com>
2023-05-08 04:17:17 +0900

    表示内容の機能を追加しました

--- shukatsu_app/home.php
+++ shukatsu_app/home.php
@@ -5,10 +5,30 @@ function H($value){
 
 require("db.php");
 
-//applicationsテーブルからデータを取得
-$result = $db->query("SELECT * FROM `shukatsu_app`");
-if (!$result) {
-    echo "表示する内容がありません";
+$fo = ['display' => ''];
+$fo['display'] = filter_input(INPUT_POST, 'display', FILTER_SANITIZE_STRING);
+
+//テーブルから表示内容を選択してデータを取得
+if ($fo['display'] == 'favorite'){
+    $result = $db->query("SELECT * FROM `shukatsu_app` WHERE `favorite`=1");
+    if (!$result) {
+        echo "表示する内容がありません";
+    }
+}elseif ($fo['display'] == 'mid'){
+    $result = $db->query("SELECT * FROM `shukatsu_app` WHERE `result`=0");
+    if (!$result) {
+        echo "表示する内容がありません";
+    }
+}elseif ($fo['display'] == 'offered'){
+    $result = $db->query("SELECT * FROM `shukatsu_app` WHERE `result`=1");
+    if (!$result) {
+        echo "表示する内容がありません";
+    }
+}else{
+    $result = $db->query("SELECT * FROM `shukatsu_app`");
+    if (!$result) {
+        echo "表示する内容がありません";
+    }
 }
 
 //取得したデータを配列に格納
@@ -38,13 +58,32 @@ while ($row = mysqli_fetch_assoc($result)) {
 		th {
 			background-color: #f2f2f2;
 		}
+        .company_1{
+			background-color: #0ff;
+        }
+        .company_2{
+			background-color: #999;
+        }
     </style>
 </head>
 <body>
     <div class="comtainer">
+        <form action="" method="post">
+            <button type="submit" name="display" value="all">すべてを表示</button>
+        </form>      
+        <form action="" method="post">
+            <button type="submit" name="display" value="favorite">気になるだけを表示</button>
+        </form>
+        <form action="" method="post">
+            <button type="submit" name="display" value="mid">選考中だけを表示</button>
+        </form>
+        <form action="" method="post">
+            <button type="submit" name="display" value="offered">内定を表示</button>
+        </form>
         <table>
             <tr>
                 <th>会社名</th>
+                <th>気になる</th>
                 <th>ES</th>
                 <th>結果</th>
                 <th>テスト</th>
@@ -54,22 +93,23 @@ while ($row = mysqli_fetch_assoc($result)) {
                 <th>結果</th>
                 <th>3次面接</th>
                 <th>結果</th>
-                <th>内定</th>
+                <th>最終結果</th>
             </tr> 
             <div class="content">
                 <?php foreach ($companies as $company) : ?>
-                    <tr>
+                    <tr class="company_<?php echo H($company['result']) ?>">
                         <td><?php echo H($company['name']); ?></td>
-                        <td><?php echo H($company['es']); ?></td>
-                        <td><?php echo H($company['es_check']); ?></td>
-                        <td><?php echo H($company['test_type']); ?></td>
-                        <td><?php echo H($company['1_interview']); ?></td>
-                        <td><?php echo H($company['1_check']); ?></td>
-                        <td><?php echo H($company['2_interview']); ?></td>
-                        <td><?php echo H($company['2_check']); ?></td>
-                        <td><?php echo H($company['3_interview']); ?></td>
-                        <td><?php echo H($company['3_check']); ?></td>
-                        <td><?php echo H($company['job_offer']); ?></td>
+                        <td><?php echo H($company['favorite']); ?></td>
+                        <td class="es"><?php echo H($company['es']); ?></td>
+                        <td class="es"><?php echo H($company['es_check']); ?></td>
+                        <td class="test"><?php echo H($company['test_type']); ?></td>
+                        <td class="int1"><?php echo H($company['1_interview']); ?></td>
+                        <td class="int1"><?php echo H($company['1_check']); ?></td>
+                        <td class="int2"><?php echo H($company['2_interview']); ?></td>
+                        <td class="int2"><?php echo H($company['2_check']); ?></td>
+                        <td class="int3"><?php echo H($company['3_interview']); ?></td>
+                        <td class="int3"><?php echo H($company['3_check']); ?></td>
+                        <td><?php echo H($company['result']); ?></td>
                     </tr>
                 <?php endforeach; ?>
             </div>
